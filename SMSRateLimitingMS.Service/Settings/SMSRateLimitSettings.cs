5d5fbb011726b86a90361196029b1f92a65a349d
oigboanugwo <igboanugwo.onyedikachi@Africaprudential.com>
2024-11-10 20:28:46 +0100

    Add configuration file

---
+++ SMSRateLimitingMS.Service/Settings/SMSRateLimitSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace SMSRateLimitingMS.Application.Settings
+{
+    public class SMSRateLimitSettings
+    {
+        public int MaxMessagesPerBusinessPhoneNumberPerSecond { get; set; }
+        public int MaxMessagesPerAccountPerSecond { get; set; }
+        //public TimeSpan InactiveThreshold { get; set; } = TimeSpan.FromHours(24);
+        //public TimeSpan CleanupInterval { get; set; } = TimeSpan.FromHours(1);
+        //public TimeSpan HistoryRetentionPeriod { get; set;} = TimeSpan.FromDays(7);
+        public TimeSpan InactiveThreshold { get; set; } = TimeSpan.FromMinutes(2);
+        public TimeSpan CleanupInterval { get; set; } = TimeSpan.FromMinutes(1);
+        public TimeSpan HistoryRetentionPeriod { get; set; } = TimeSpan.FromMinutes(3);
+    }
+}
